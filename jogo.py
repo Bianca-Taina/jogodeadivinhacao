e0f4cb44737b789faa9c642df1f5ba7e82447e28
Bianca-Taina <bianca.balduino@escola.pr.gov.br>
2025-07-31 12:17:47 +0000

    primeira parte do jogo

---
+++ jogo.py
@@ -0,0 +1,9 @@
+import random
+print("&&&&&&&&&&&&&&&&&&&&&&&&&&&")
+print("🪽 jogo de adivinhação   🪽") 
+print("🪽  Bianca               🪽")
+print("&&&&&&&&&&&&&&&&&&&&&&&&&&&")
+
+numeroSecreto= random.randrange(0,100)
+totalTentativas = 0 
+pontos = 100
